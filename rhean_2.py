cb09f30989b00f151038fee6df2f77e2078f4005
rentapao0 <rentapao0@gmail.com>
2026-02-15 16:12:28 +0800

    Add files via upload

---
+++ rhean_2.py
@@ -0,0 +1,16 @@
+fullname = "Rhean Tapao"
+age = 18
+height = 1.67
+currently_working = True
+
+print("fullname:", fullname)
+print(type(fullname))
+      
+print("\nAge:", age)
+print(type(age))
+
+print("\nHeight:", height)
+print(type(height))
+
+print("\nCurrently working:", currently_working)
+print(type(currently_working))
